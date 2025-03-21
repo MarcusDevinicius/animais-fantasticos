4ac2635a14ecda8127214590b630191b5b3a5986
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-03-21 11:15:12 -0300

    transição initAnimacao

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/srcoll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
@@ -10,11 +9,12 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetchanimais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 const scrollsuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]'); //COm isso qualque pesssoa poderá importar essa classe com esse método de scroll suave para seu site.
 scrollsuave.init();
 
-initAnimacaoScroll();
+
 initAccordion();
 initTabNav();
 initModal();
@@ -24,6 +24,7 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
 
 // import $ from 'jquery';
 // import _ from 'lodash';
