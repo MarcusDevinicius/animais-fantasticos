40e9fd4c182b0a6041f3969fb8a85f981ab1e582
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-03-26 17:23:15 -0300

    refatoração tabnav

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,21 +1,35 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  function activeTab(index) {
+export default class TabNav {
+  constructor(tabMenu, tabContent) {
+    this.tabMenu = tabMenu;
+    this.tabContent = tabContent;
+    this.activClass = 'ativo';
+  }
+  // const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  // const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  tabNavInit() {
+    if (tabMenu.length && tabContent.length) {
+      tabContent[0].classList.add(this.activClass);
+      this.tabMenu.forEach((li, index) => {
+        li.addEventListener("click", () => {
+          this.activeTab(index);
+        });
+      });
+    }
+  }
+  activeTab(index) {
     tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+      section.classList.remove(this.activClass);
     });
     const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add("ativo", direcao);
-  }
-
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
-
-    tabMenu.forEach((li, index) => {
-      li.addEventListener("click", () => {
-        activeTab(index);
-      });
-    });
+    tabContent[index].classList.add(this.activClass, direcao);
   }
 }
+// if (tabMenu.length && tabContent.length) {
+//   tabContent[0].classList.add("ativo");
+
+//   // tabMenu.forEach((li, index) => {
+//   //   li.addEventListener("click", () => {
+//   //     activeTab(index);
+//   //   });
+//   // });
+// }
