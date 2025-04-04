53f173c44326c53a2ab69de2550b8be44cfaba63
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-04-04 11:56:54 -0300

    animação ao scroll refeita

--- js/modules/scrollAnima.js
+++ js/modules/scrollAnima.js
@@ -3,24 +3,44 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - this.windowMetade;
-      if (sectionTop < 0) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+
+  // pega a disdancia de cada section
+  //em relação ao scroll
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  //verifica a distancia em cada objeto
+  //em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    window.addEventListener('scroll', this.animaScroll);
-    this.sections[0].classList.add('ativo');
-
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
     return this;
+  }
+  //remove event de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
