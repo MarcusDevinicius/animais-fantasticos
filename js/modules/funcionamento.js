5f9b2ab3e98a3fb59acfa5936c3c9acc19664be3
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-04-05 10:04:15 -0300

    funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,29 +1,43 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector("[data-semana]");
-  const diasSemana = funcionamento.dataset.semana.split(", ").map(Number); //Ao passar o split a string se torna Array, e passando o map(Number) retorna uma nova Array, porém com os valores em number
-  const horarioSemana = funcionamento.dataset.horario.split(", ").map(Number);
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horaAgora = dataAgora.getHours();
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(", ").map(Number); //Ao passar o split a string se torna Array, e passando o map(Number) retorna uma nova Array, porém com os valores em number
+    this.horarioSemana = this.funcionamento.dataset.horario
+      .split(", ")
+      .map(Number);
+  }
 
-  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1; //se um elemento nao está na Array retorna-se -1;
-  const horaAberto =
-    horaAgora >= horarioSemana[0] && horaAgora < horarioSemana[1];
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = dataAgora.getDay();
+    this.horaAgora = dataAgora.getUTCHours() - 3; //horario universal(- 3 por que é o calculo em relação ao Brasil)
+  }
 
-  if (semanaAberto && horaAberto) {
-    funcionamento.classList.add("aberto");
+  estaAberto() {
+    const semanaAberto = this.diasSemana.indexOf(diaAgora) !== -1; //se um elemento nao está na Array retorna-se -1;
+    const horaAberto =
+      this.horaAgora >= horarioSemana[0] &&
+      this.horaAgora < this.horarioSemana[1];
+
+    return semanaAberto && horaAberto;
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.ativaAberto();
+      this.dadosFuncionamento();
+      this.dadosAgora();
+    }
+    return this;
   }
 }
-
-const agora = new Date();
-const futuro = new Date("Dec 24 2025");
-
-function transformarHoras(tempo) {
-  return tempo / (24 * 60 * 60 * 1000); //transformando ms para dias
-}
-
-const diasAgora = transformarHoras(agora.getTime()); //ms de 1970 até agora
-const diasFuturo = transformarHoras(futuro.getTime());
-
-console.log(Math.floor(diasFuturo) - Math.floor(diasAgora));
